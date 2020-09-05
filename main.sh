aac9c3d3e9dc77a75e835d9e8353f50523453387
Emmanuel Valverde Ramos <evrtrabajo@gmial.com>
2020-09-05 11:17:35 +0200

    Added morning function to check git changes

--- main.sh
+++ main.sh
@@ -3,7 +3,6 @@ echo "▶️ Making symbolic links of the dotfiles"
 FILES=$(find ${BASH_SOURCE%/*} -maxdepth 1 -type f -name '.[^.]*' ! -name "main.sh" -print0 | sort -z | xargs -r0)
 for f in $FILES; do
 	[ -f ~/$f  ] && rm -f ~/$f
-	ln -s $(pwd)/$f ~/$f || break # execute successfully or break
-  # Or more explicitly: if this execution fails, then stop the `for`:
-  # if ! bash "$f" -H; then break; fi
+	ln -s $(pwd)/$f ~/$f || break
 done
+mkdir -p ~/projects
